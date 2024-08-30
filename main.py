f48016c38c5d66537aea3e1158cd1ae5fdd805fd
MaikeRob <128054364+MaikeRob@users.noreply.github.com>
2024-08-29 22:01:53 -0300

    Update main.py

--- main.py
+++ main.py
@@ -8,5 +8,13 @@ temperatura = ctrl.Antecedent(np.arange(0, 36, 1), "temperatura")
 ergonomia = ctrl.Antecedent(np.arange(0, 101, 1), "ergonomia")
 qualidade_equipamento = ctrl.Antecedent(np.arange(0, 101, 1), "qualidade_equipamento")
 
-iluminacao["claro"]
-iluminacao["claro"]
+iluminacao["claro"] = fuzz.trimf(iluminacao.universe, [40, 100, 100])
+iluminacao["escuro"] = fuzz.trimf(iluminacao.universe, [0, 0, 60])
+
+temperatura["quente"] = fuzz.trapmf(temperatura.universe, [0, 0, 10, 20])
+temperatura["ambiente"] = fuzz.trimf(temperatura.universe, [13, 22, 29])
+temperatura["frio"] = fuzz.trapmf(temperatura.universe, [26, 30, 35, 35])
+
+ergonomia["confortavel"] = fuzz.gaussmf(ergonomia.universe, 100, 20)
+ergonomia["desconfortavel"] = fuzz.gaussmf(ergonomia.universe, 0, 20)
+
